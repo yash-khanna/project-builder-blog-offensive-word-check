a4be0fafe3a81081b11e6a66c577283d510520d3
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-26 13:49:56 +0530

    Create TestRegistrationValidation.java

---
+++ src/testing/TestRegistrationValidation.java
@@ -0,0 +1,31 @@
+package testing;
+
+/*Uncomment the below code to test your application
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import businesslogic.RegistrationValidation;
+
+public class TestRegistrationValidation {
+	RegistrationValidation rv = new RegistrationValidation();
+	
+	@Test
+	public void testCheckUserDetails() {
+		
+		assertEquals(true, rv.checkUserDetails("amin@gmail.com", "Amin@556", "Amin@556"));
+		assertEquals(false, rv.checkUserDetails("amingmail.com", "Amin@556", "Amin@556"));
+		assertEquals(false, rv.checkUserDetails("amin@gmail.com", "Amin556", "Amin@556"));
+		assertEquals(false, rv.checkUserDetails("amin@gmail.com", "Amin@556", "Amin556"));
+		assertEquals(false, rv.checkUserDetails("amin@gmail.com", "Amin556", "Amin556"));
+		assertEquals(false, rv.checkUserDetails("amin@gmail.com", "amin@556", "amin@556"));
+		assertEquals(false, rv.checkUserDetails("amin@gmail.com", "Amin@am", "Amin@am"));
+		try {
+			rv.checkUserDetails(null, null, null);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
+*/
