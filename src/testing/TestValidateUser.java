6bb386bc4831a7179852e517388d4a11487baa2c
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-26 13:53:35 +0530

    Create TestValidateUser.java

---
+++ src/testing/TestValidateUser.java
@@ -0,0 +1,27 @@
+package testing;
+
+/*Uncomment the below code to test your application
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import businesslogic.ValidateUser;
+
+public class TestValidateUser {
+	ValidateUser vu = new ValidateUser();
+	
+	@Test
+	public void testValidate() {
+		assertEquals(false,vu.validate("Amin@gmail.com","Amin@556"));
+		assertEquals(false,vu.validate("admin", "admin1"));
+		assertEquals(false,vu.validate("admin1", "admin"));
+		assertEquals(true,vu.validate("admin", "admin"));
+		try {
+			vu.validate(null, null);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+*/
